dc682141c49d1e914e46ca80109bc20ff8595677
Alex <AlexAngelosky@users.noreply.github.com>
2014-06-21 22:47:49 +0300

    Update exam_generator.cpp

--- exam_generator.cpp
+++ exam_generator.cpp
@@ -24,6 +24,25 @@ void num_generator(int diff,unsigned int &a,unsigned int &b){
        }
 }
 
+void num_generator(int diff,unsigned int &a){
+       srand(time(NULL));
+       a = 0;
+       char temp = 0;
+       if(diff<3){
+          temp = rand()%6+10;
+          if(diff == 2) a = rand()%15+1;
+          else if(diff == 1) a = temp;
+          a = a <<8;
+          if(diff == 2) a+= rand()%15+1;
+          else if(diff == 1) a+= temp;          
+          a = a <<4;
+       }
+       else{
+          a = rand();
+       }
+}
+
+
 int main(){
 
   return 0;
